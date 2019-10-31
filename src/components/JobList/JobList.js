a01f77e6bafe5711d5a4335f39bfdf512f4015d1
Khachatour <8447007+Khachatour@users.noreply.github.com>
2019-10-31 13:52:51 +0400

    core: add urgency toggle

--- src/components/JobList/JobList.js
+++ src/components/JobList/JobList.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import Card from '@material-ui/core/Card'
 import Button from '@material-ui/core/Button'
+import Switch from '@material-ui/core/Switch'
 import { getAllJobs } from '../../redux/selectors'
 import { makeStyles } from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography'
@@ -36,6 +37,8 @@ const useStyles = makeStyles({
 
 const JobList = () => {
   const jobs = useSelector(getAllJobs)
+  const [urgencyStatus, setUrgencyStatus] = useState(false)
+  const setUrgency = () => setUrgencyStatus(!urgencyStatus)
   const classes = useStyles()
 
   if (!jobs) {
@@ -43,45 +46,57 @@ const JobList = () => {
   }
 
   return (
-    <div className="job-list-wrapper">
-      {jobs.map((job, idx) => (
-        <Card className={classes.card} key={idx}>
-          <CardContent>
-            <Typography
-              className={classes.title}
-              color="textSecondary"
-              gutterBottom
-            >
-              {job.location}
-            </Typography>
-            <Typography variant="h5" component="h2">
-              {job.title}
-            </Typography>
-            <Typography color="textSecondary" className={classes.urgency}>
-              Urgency status:{' '}
-              <span className={job.urgent ? classes.urgent : ''}>
-                {job.urgent ? 'Urgent' : 'Not urgent'}
-              </span>
-            </Typography>
-            <Typography variant="body2" component="p">
-              Bonus: {job.bonus}
-            </Typography>
+    <>
+      <div className="job-list-filter">
+        Show urgent jobs:
+        <Switch
+          value="urgencyStatus"
+          onChange={setUrgency}
+          checked={urgencyStatus}
+        />
+      </div>
+      <div className="job-list-wrapper">
+        {jobs
+          .filter(job => (urgencyStatus ? job.urgent : job))
+          .map((job, idx) => (
+            <Card className={classes.card} key={idx}>
+              <CardContent>
+                <Typography
+                  className={classes.title}
+                  color="textSecondary"
+                  gutterBottom
+                >
+                  {job.location}
+                </Typography>
+                <Typography variant="h5" component="h2">
+                  {job.title}
+                </Typography>
+                <Typography color="textSecondary" className={classes.urgency}>
+                  Urgency status:{' '}
+                  <span className={job.urgent ? classes.urgent : ''}>
+                    {job.urgent ? 'Urgent' : 'Not urgent'}
+                  </span>
+                </Typography>
+                <Typography variant="body2" component="p">
+                  Bonus: {job.bonus}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Link to={`/job/${job.id}`} className="link">
+                  <Button variant="contained" size="small">
+                    View Job
+                  </Button>
+                </Link>
+              </CardActions>
+            </Card>
+          ))}
+        <Card className={classes.card}>
+          <CardContent className="add-job" onClick={console.log}>
+            +
           </CardContent>
-          <CardActions>
-            <Link to={`/job/${job.id}`} className="link">
-              <Button variant="contained" size="small">
-                View Job
-              </Button>
-            </Link>
-          </CardActions>
         </Card>
-      ))}
-      <Card className={classes.card}>
-        <CardContent className="add-job" onClick={console.log}>
-          +
-        </CardContent>
-      </Card>
-    </div>
+      </div>
+    </>
   )
 }
 
